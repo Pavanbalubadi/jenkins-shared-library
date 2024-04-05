7b62dbc7e53e5f17152eab0ef61a39fea5a0bc1c
pavan <balubadipavan@gmail.com>
2024-04-05 08:11:19 +0530

    learn jenkins shared lib

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,26 +1,28 @@
 
 //stage('code checkout') {}
 //stage('compile/download dependencies') {}
-//
-//if(env.BRANCH_NAME == "main"){
-//    stage('Build') {}
-//}else if (env.BRANCH_NAME ==~ "PR.*"){
-//    stage('integration test') {}
-//}else if (env.TAG_NAME ==~ ".*"){
-//    stage('Build') {}
-//    stage('Release') {}
-//}else {
-//    stage('test cases') {}
-//}
+//stage('test cases') {}
+//stage('integration test') {}
+//stage('Build') {}
+//stage('Release') {}
 def call() {
     node('workstation') {
         sh 'env'
-        stage('code checkout') {}
+
+stage('code checkout') {}
 stage('compile/download dependencies') {}
-stage('test cases') {}
-stage('integration test') {}
-stage('Build') {}
-stage('Release') {}
+
+if(env.BRANCH_NAME == "main"){
+    stage('Build') {}
+}else if (env.BRANCH_NAME ==~ "PR.*"){
+    stage('integration test') {}
+}else if (env.TAG_NAME ==~ ".*"){
+    stage('Build') {}
+    stage('Release') {}
+}else {
+    stage('test cases') {}
+}
+
 
     }
 
